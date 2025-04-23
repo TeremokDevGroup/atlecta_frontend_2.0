660e3fa1dca77b2a852a98ecf05621e743e2a8e7
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-04-23 21:24:41 +0700

    chore: add Tailwind CSS and configure PostCSS for styling

---
+++ atlecta_frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/components/LoginForn.tsx",
+    "./src/components/RegisterForm.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
