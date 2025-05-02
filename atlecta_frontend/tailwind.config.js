1d1ba32c5bc8f82a5cc30705d4ad5060b3cecefc
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-05-02 22:43:37 +0700

    chore: added components to Tailwind CSS

--- atlecta_frontend/tailwind.config.js
+++ atlecta_frontend/tailwind.config.js
@@ -6,7 +6,9 @@ export default {
     "./src/components/RegisterForm.tsx",
     "./src/components/YandexMap.tsx",
     "./src/components/MapForm.tsx",
+    "./src/components/ProfileForm.tsx",
     "./src/pages/YandexMapPage.tsx",
+    "./src/pages/ProfilePage.tsx",
   ],
   theme: {
     extend: {},
