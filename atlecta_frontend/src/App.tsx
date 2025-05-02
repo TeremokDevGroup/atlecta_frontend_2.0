5322899ef14c3998c3444f7f8a5fe13b36e101d7
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-05-02 22:44:44 +0700

    feat: add route for Profile Page and update App component

--- atlecta_frontend/src/App.tsx
+++ atlecta_frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 import { YandexMapPage } from './pages/YandexMapPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/map" element={<YandexMapPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
