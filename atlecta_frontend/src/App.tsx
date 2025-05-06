78ca1ff97f504b6891b9d8de66f3ca08d446af2e
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-05-07 02:39:05 +0700

    feat: Update App.tsx to handle private routes and integrate ProtectedRoute

--- atlecta_frontend/src/App.tsx
+++ atlecta_frontend/src/App.tsx
@@ -1,21 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import { YandexMapPage } from "./pages/YandexMapPage"; 
-import ProfilePage from "./pages/ProfilePage"; 
-import { LoginPage } from "./pages/LoginPage"; 
-import { RegisterPage } from "./pages/RegisterPage"; 
-import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { routes } from "./routes/routesConfig";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="map" element={<YandexMapPage />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="*" element={<NotFound />} />
+          {routes.map(({ path, element, private: isPrivate }) => (
+            <Route
+              key={path}
+              path={path}
+              element={isPrivate ? <ProtectedRoute>{element}</ProtectedRoute> : element}
+            />
+          ))}
         </Route>
       </Routes>
     </BrowserRouter>
