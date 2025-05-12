a8efb97c64d923b37708b585054ae3737c4c085a
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-05-12 16:35:40 +0700

    types: add UserImage and extended user profile types

--- atlecta_frontend/src/types/user.ts
+++ atlecta_frontend/src/types/user.ts
@@ -1,15 +1,19 @@
-export interface Sport {
-    name: string;
+export interface UserProfile {
+  first_name: string;
+  last_name: string;
+  age: number;
+  height: number;
+  weight: number;
+  gender: number;
+  sports: { name: string }[];
+  user_id: string;
+  bio: string;
+  profile_picture?: string;
 }
 
-export interface UserProfile {
-     first_name :  string;
-   last_name :  string;
-   age :  number;
-   height :  number;
-   weight :  number;
-   gender :  number;
-   sports : Sport[];
-   user_id :  string;
-   bio :  string;
+export interface UserImage {
+  url: string;
+  id: string;
+  is_profile_picture: boolean;
+  uploaded_at: string;
 }
