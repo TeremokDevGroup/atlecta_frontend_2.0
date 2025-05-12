37ec9e1855b83b2ee4ef857a23a7d0aaaa296693
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-05-12 16:33:26 +0700

    refactor: delete comment

--- atlecta_frontend/src/pages/ProfilePage.tsx
+++ atlecta_frontend/src/pages/ProfilePage.tsx
@@ -3,7 +3,7 @@ import { ProfileForm } from '../components/ProfileForm';
 export default function ProfilePage() {
   return (
     <div className="py-10 px-4">
-      <h1 className="text-2xl font-bold mb-6">Профиль</h1> //#TODO finish Put request since the API is not implemented
+      <h1 className="text-2xl font-bold mb-6">Профиль</h1> 
       <ProfileForm />
     </div>
   );
