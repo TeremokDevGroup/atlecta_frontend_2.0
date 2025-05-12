65eb7348edf6856e96149d836ea7e9b88874e5a2
ЛАПИН СЕРГЕЙ <lapin.yira@gmail.com>
2025-05-12 16:34:29 +0700

    feat: add routes for profile and feed pages

--- atlecta_frontend/src/routes/routesConfig.tsx
+++ atlecta_frontend/src/routes/routesConfig.tsx
@@ -1,14 +1,17 @@
-// src/routes/routesConfig.ts
 import YandexMapPage from "../pages/YandexMapPage";
 import ProfilePage from "../pages/ProfilePage";
+import NotFound from "../pages/NotFound";
+import FeedPage from "../pages/FeedPage";
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
-import NotFound from "../pages/NotFound";
+import { UserProfilePage } from "../pages/UserProfilePage";
 
 export const routes = [
-  { path: "map", element: <YandexMapPage />, private: true },
-  { path: "profile", element: <ProfilePage />, private: true },
+  { path: "map", element: <YandexMapPage />, private: false },
   { path: "login", element: <LoginPage />, private: false },
   { path: "register", element: <RegisterPage />, private: false },
+  { path: "profile", element: <ProfilePage />, private: true },
+  { path: "users/profiles", element: <FeedPage />, private: true },
+  { path: "/profile/:user_id", element: <UserProfilePage />, private: true },
   { path: "*", element: <NotFound />, private: false },
 ];
