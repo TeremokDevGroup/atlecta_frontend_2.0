d40280a89f9c2900a896b6600a547845557ee532
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2025-05-27 13:22:15 +0700

    deploy: dockerized application for testing (not producion version)
    - Added Dockerfile
    - Added --host flag to vite
    - Added server.allowedHosts to vite config

--- atlecta_frontend/vite.config.ts
+++ atlecta_frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // cors: {
+    // 	origin: ['https://mysubdomain.domain.io', 'http://localhost:5173'],
+    // 	methods: ['GET', 'POST'],
+    // 	allowedHeaders: ['Content-Type']
+    // },
+    allowedHosts: ['atlecta.ru'] //added this
+  }
+});
